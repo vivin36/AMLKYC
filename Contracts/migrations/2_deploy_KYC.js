4c82657f943c464c8a09a23eef8764e604b473a2
Abhinaba.Das@xe04.ey.com <Abhinaba.Das@xe04.ey.com>
2018-11-02 19:12:02 +0530

    Conflicts during Truffle testing resolved

--- Contracts/migrations/2_deploy_KYC.js
+++ Contracts/migrations/2_deploy_KYC.js
@@ -2,5 +2,7 @@ const KYC = artifacts.require("./KYC.sol");
 const ScreeningList = artifacts.require('./ScreeningList.sol');
 
 module.exports = function(deployer) {
-  deployer.deploy(KYC,ScreeningList.address);
+  deployer.deploy(ScreeningList).then(function() {
+    return deployer.deploy(KYC,ScreeningList.address);
+  });  
 };

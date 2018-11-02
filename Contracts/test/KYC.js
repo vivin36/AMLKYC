ddcc946c8c71042c05edc9f53f56a995dceb720c
Vivin Prasannaa <Vivin.Prasannaa@gds.ey.com>
2018-11-02 17:47:27 +0530

    Contract test cases and meaning name change in deploying contract

--- Contracts/test/KYC.js
+++ Contracts/test/KYC.js
@@ -1,26 +1,29 @@
 const KYC = artifacts.require("KYC");
+const ScreeningList = artifacts.require('ScreeningList');
 
 contract('KYC', async (accounts) => {
     let KYCContract;
+    let screeningList;
 
     before(async () => {
-        KYCContract = await KYC.new();
+        screeningList = await ScreeningList.new();
+        KYCContract = await KYC.new(screeningList.contract.address);
     }); 
 
     it("Create a KYC document in Blockchain and verifying it", async () => {
-        await KYCContract.createKYC("0x12345","vivin","North Down Street","03/06/1994","M",1572432745);
+        await KYCContract.createKYC("0x12345","vivin",0,0,true,1572432745);
         let response = await KYCContract.getDetailsByID("0x12345");
         assert.equal(response[0], "vivin");
     })
 
-    it("Get validity for a KYC document by id", async () => {
+    it("Get validity for a KYC document by account Number", async () => {
         let response = await KYCContract.getValidity("0x12345");
         assert.equal(response, 1572432745);
     })
 
     it("Update an existing KYC document in Blockchain and verifying it", async() => {
-        await KYCContract.updateKYC("0x12345","vivin","Middleton Street","03/06/1994","M",1572432745);
+        await KYCContract.updateKYC("0x12345","Prasannaa",1,1,true,1572432745);
         let response = await KYCContract.getDetailsByID("0x12345");
-        assert.equal(response[1], "Middleton Street", "Failed to updated KYC document");
+        assert.equal(response[0], "Prasannaa", "Failed to updated KYC document");
     })
 })

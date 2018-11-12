ea8a73c79c13892cf14bef34c3bd1464aab497e8
Abhinaba.Das@xe04.ey.com <Abhinaba.Das@xe04.ey.com>
2018-11-12 15:25:26 +0530

    Screening List Contract test cases

--- Contracts/test/ScreeningList.js
+++ Contracts/test/ScreeningList.js
@@ -9,67 +9,93 @@ contract('ScreeningList', () => {
 
     it('Should be able to add a black listed customer', async() => {
         await screeningList.addBlackListedCustomer(
-            'BX23456765',
-            'Arjun'
+            '0x83caa84d4d5a34d217c4aba7d7ae938c95a86451',
+            'TY678721',
+            'Arunima'
         );
-    });
-
-    it('Should be able to retrieve a black listed customer', async() => {
-        let response = await screeningList.getBlackListedCustomer('BX23456765');
-        assert.equal(web3.toAscii(response).replace(/\0/g, ''), "Arjun", "Error in retrieving a black listed customer");
-    });
-
-    it('Should be able to check whether a customer is black listed or not', async() => {
-        let response = await screeningList.checkIsBlackListed('BX23456765');
-        assert.equal(response, true, "Error in checking a black listed customer");
     });
 
     it('Should be able to add another black listed customer', async() => {
         await screeningList.addBlackListedCustomer(
-            'TY56743498',
-            'Paul'
+            '0xb26ba10df8fa266f6bfef127b0c11b1ff458fe25',
+            'JP4567875',
+            'Arjun'
         );
     });
 
-    it('Should be able to remove a customer from black list', async() => {
-        await screeningList.removeBlackListedCustomer('BX23456765');
+    it('Should be able to check whether a customer is black listed or not', async() => {
+        let response = await screeningList.checkIsBlackListed('0x83caa84d4d5a34d217c4aba7d7ae938c95a86451');
+        assert.equal(response, true, "Error in checking a black listed customer");
+    });
+
+    it('Should be able to retrieve all black listed customers', async() => {
+        let response = await screeningList.getBlackListedCustomers();
+        let addressesList = response[0];
+        let identificationsList = response[1];
+        let namesList = response[2];
+
+        assert.equal(addressesList[0], "0x83caa84d4d5a34d217c4aba7d7ae938c95a86451");
+        assert.equal(addressesList[1], "0xb26ba10df8fa266f6bfef127b0c11b1ff458fe25");
+
+        assert.equal(web3.toAscii(identificationsList[0]).replace(/\0/g, ''), "TY678721");
+        assert.equal(web3.toAscii(identificationsList[1]).replace(/\0/g, ''), "JP4567875");
+
+        assert.equal(web3.toAscii(namesList[0]).replace(/\0/g, ''), "Arunima");
+        assert.equal(web3.toAscii(namesList[1]).replace(/\0/g, ''), "Arjun");
+    });
+
+    it('Should be able to remove a black listed customer', async() => {
+        await screeningList.removeBlackListedCustomer("0x83caa84d4d5a34d217c4aba7d7ae938c95a86451");
     });
 
     it('Should be able to check the removed customer does not exist in black list anymore', async() => {
-        let response = await screeningList.checkIsBlackListed('BX23456765');
+        let response = await screeningList.checkIsBlackListed('0x83caa84d4d5a34d217c4aba7d7ae938c95a86451');
         assert.equal(response, false, 'Error in checking a removed black list customer');
     });
     
     it('Should be able to add a white listed customer', async() => {
         await screeningList.addWhiteListedCustomer(
-            'JKL907653',
+            '0x77c52ede53ceb63b0e9924f4c80ef47ee5049ac3',
+            'CDE98756',
             'Dmitri'
         );
     });
 
-    it('Should be able to retrieve a white listed customer', async() => {
-        let response = await screeningList.getWhiteListedCustomer('JKL907653');
-        assert.equal(web3.toAscii(response).replace(/\0/g, ''), "Dmitri", "Error in retrieving a white listed customer");
-    });
-
-    it('Should be able to check whether a customer is white listed or not', async() => {
-        let response = await screeningList.checkIsWhiteListed('JKL907653');
-        assert.equal(response, true, "Error in checking a white listed customer");
-    });
-
     it('Should be able to add another white listed customer', async() => {
         await screeningList.addWhiteListedCustomer(
-            'DFG567421',
+            '0x58c4ae5aea5c92b4f568712caae8e1731a2e7ab2',
+            'RTY90076',
             'Sean'
         );
     });
 
+    it('Should be able to check whether a customer is white listed or not', async() => {
+        let response = await screeningList.checkIsWhiteListed('0x58c4ae5aea5c92b4f568712caae8e1731a2e7ab2');
+        assert.equal(response, true, "Error in checking a white listed customer");
+    });
+
+    it('Should be able to retrieve all white listed customers', async() => {
+        let response = await screeningList.getWhiteListedCustomer();
+        let addressesList = response[0];
+        let identificationsList = response[1];
+        let namesList = response[2];
+
+        assert.equal(addressesList[0], "0x77c52ede53ceb63b0e9924f4c80ef47ee5049ac3");
+        assert.equal(addressesList[1], "0x58c4ae5aea5c92b4f568712caae8e1731a2e7ab2");
+
+        assert.equal(web3.toAscii(identificationsList[0]).replace(/\0/g, ''), "CDE98756");
+        assert.equal(web3.toAscii(identificationsList[1]).replace(/\0/g, ''), "RTY90076");
+
+        assert.equal(web3.toAscii(namesList[0]).replace(/\0/g, ''), "Dmitri");
+        assert.equal(web3.toAscii(namesList[1]).replace(/\0/g, ''), "Sean");
+    });
+
     it('Should be able to remove a customer from white list', async() => {
-        await screeningList.removeBlackListedCustomer('JKL907653');
+        await screeningList.removeWhiteListedCustomer('0x77c52ede53ceb63b0e9924f4c80ef47ee5049ac3');
     });
 
     it('Should be able to check the removed customer does not exist in white list anymore', async() => {
-        let response = await screeningList.checkIsBlackListed('JKL907653');
+        let response = await screeningList.checkIsWhiteListed('0x77c52ede53ceb63b0e9924f4c80ef47ee5049ac3');
         assert.equal(response, false, 'Error in checking a removed black list customer');
     });
 });

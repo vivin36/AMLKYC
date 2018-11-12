3c7c7ccf7de14b518ef9bbb56bd5c843a69e3ef6
Vivin Prasannaa <Vivin.Prasannaa@gds.ey.com>
2018-11-12 17:43:01 +0530

    Screening List api to add,remove,list whitelist and blacklist customers

--- api/src/business/screeningList.service.js
+++ api/src/business/screeningList.service.js
@@ -5,41 +5,40 @@ import config from '../config/config';
 const web3 = Web3();
 const screeningListInstance = new web3.eth.Contract(screeningListMetaData.abi, screeningListMetaData.networks[config.development.network_id].address);
 
-let blackListCustomer = async (accountNumber, name,identificationNumber) => {
-    try {    
+let blackListCustomer = async (accountNumber, name, identificationNumber) => {
+    try {
         const key = config.nodeFour.key;
         const accounts = await web3.eth.getAccounts();
         await screeningListInstance.methods.addBlackListedCustomer(
-            accountNumber,
-            web3.utils.fromAscii(name),
-            web3.utils.fromAscii(identificationNumber))
+                accountNumber,
+                web3.utils.fromAscii(name),
+                web3.utils.fromAscii(identificationNumber))
             .send({
                 from: accounts[0],
                 gas: 4700000
             });
-        return Promise.resolve();    
+        return Promise.resolve();
     } catch (err) {
-        console.log('error',err);
         return Promise.reject(err);
     }
 };
 
 let getAllBlackListedCustomers = async () => {
-    try {  
+    try {
         let BlackListedCustomersList = [];
         let BlackListedCustomers = {};
 
         const customerResponse = await screeningListInstance.methods.getBlackListedCustomers().call();
         const addressesList = customerResponse[0].filter(elem => elem != '0x0000000000000000000000000000000000000000');
-        const  namesList = customerResponse[1].filter(elem => elem != '0x0000000000000000000000000000000000000000');
-        const identificationNumbersList  = customerResponse[2].filter(elem => elem != '0x0000000000000000000000000000000000000000');
+        const namesList = customerResponse[1].filter(elem => elem != '0x0000000000000000000000000000000000000000');
+        const identificationNumbersList = customerResponse[2].filter(elem => elem != '0x0000000000000000000000000000000000000000');
 
         for (var count = 0; count < addressesList.length; count++) {
-            BlackListedCustomers.address =  addressesList[count];   
+            BlackListedCustomers.address = addressesList[count];
             BlackListedCustomers.identificationNumber = web3.utils.toAscii(identificationNumbersList[count]).replace(/\0/g, '');
             BlackListedCustomers.name = web3.utils.toAscii(namesList[count]).replace(/\0/g, '');
             BlackListedCustomersList.push(BlackListedCustomers);
-            BlackListedCustomers = {};            
+            BlackListedCustomers = {};
         }
 
         return Promise.resolve(BlackListedCustomersList);
@@ -48,41 +47,41 @@ let getAllBlackListedCustomers = async () => {
     }
 };
 
-let whiteListCustomer = async (accountNumber, name,identificationNumber) => {
-    try {    
+let whiteListCustomer = async (accountNumber, name, identificationNumber) => {
+    try {
         const key = config.nodeFour.key;
         const accounts = await web3.eth.getAccounts();
         await screeningListInstance.methods.addWhiteListedCustomer(
-            accountNumber,
-            web3.utils.fromAscii(name),
-            web3.utils.fromAscii(identificationNumber))
+                accountNumber,
+                web3.utils.fromAscii(name),
+                web3.utils.fromAscii(identificationNumber))
             .send({
                 from: accounts[0],
                 gas: 4700000
             });
-        return Promise.resolve();    
+        return Promise.resolve();
     } catch (err) {
-        console.log('error',err);
+        console.log('error', err);
         return Promise.reject(err);
     }
 };
 
 let getAllwhiteListCustomers = async () => {
-    try {  
+    try {
         let BlackListedCustomersList = [];
         let BlackListedCustomers = {};
 
         const customerResponse = await screeningListInstance.methods.getWhiteListedCustomer().call();
         const addressesList = customerResponse[0].filter(elem => elem != '0x0000000000000000000000000000000000000000');
-        const  namesList = customerResponse[1].filter(elem => elem != '0x0000000000000000000000000000000000000000');
-        const identificationNumbersList  = customerResponse[2].filter(elem => elem != '0x0000000000000000000000000000000000000000');
+        const namesList = customerResponse[1].filter(elem => elem != '0x0000000000000000000000000000000000000000');
+        const identificationNumbersList = customerResponse[2].filter(elem => elem != '0x0000000000000000000000000000000000000000');
 
         for (var count = 0; count < addressesList.length; count++) {
-            BlackListedCustomers.address =  addressesList[count];   
+            BlackListedCustomers.address = addressesList[count];
             BlackListedCustomers.identificationNumber = web3.utils.toAscii(identificationNumbersList[count]).replace(/\0/g, '');
             BlackListedCustomers.name = web3.utils.toAscii(namesList[count]).replace(/\0/g, '');
             BlackListedCustomersList.push(BlackListedCustomers);
-            BlackListedCustomers = {};            
+            BlackListedCustomers = {};
         }
 
         return Promise.resolve(BlackListedCustomersList);
@@ -91,9 +90,52 @@ let getAllwhiteListCustomers = async () => {
     }
 };
 
+
+let checkIsWhiteListed = async (accountAddress) => {
+    try {
+        const screeningListResponse = await screeningListInstance.methods.checkIsWhiteListed(accountAddress).call();
+        let response = {
+            isWhiteList: screeningListResponse
+        }
+        return Promise.resolve(response);
+    } catch (err) {
+        return Promise.reject(err);
+    }
+};
+
+let removeFromBlackListedCustomer = async (accountAddress) => {
+    try {
+        const accounts = await web3.eth.getAccounts();
+        const screeningListResponse = await screeningListInstance.methods.removeBlackListedCustomer(accountAddress).send({
+            from: accounts[0],
+            gas: 4700000
+        });
+        return Promise.resolve();
+    } catch (err) {
+        return Promise.reject(err);
+    }
+};
+
+let removeFromWhiteListedCustomer = async (accountAddress) => {
+    try {
+        const accounts = await web3.eth.getAccounts();
+        const screeningListResponse = await screeningListInstance.methods.removeWhiteListedCustomer(accountAddress).send({
+            from: accounts[0],
+            gas: 4700000
+        });
+        return Promise.resolve();
+    } catch (err) {
+        return Promise.reject(err);
+    }
+};
+
+
 export {
     blackListCustomer,
     getAllBlackListedCustomers,
     whiteListCustomer,
-    getAllwhiteListCustomers
+    getAllwhiteListCustomers,
+    checkIsWhiteListed,
+    removeFromBlackListedCustomer,
+    removeFromWhiteListedCustomer
 };
--- api/src/routes/screeningList.js
+++ api/src/routes/screeningList.js
@@ -1,14 +1,13 @@
 import express from 'express';
 const {
-  blackListCustomer,getAllBlackListedCustomers,whiteListCustomer,getAllwhiteListCustomers
-} = require('../business/screeningList.service');
+  blackListCustomer,getAllBlackListedCustomers,whiteListCustomer,getAllwhiteListCustomers,checkIsWhiteListed
+,removeFromBlackListedCustomer,removeFromWhiteListedCustomer} = require('../business/screeningList.service');
 
 const router = express.Router();
 
-router.post('/blacklistedCustomers', async (req, res) => {
+router.post('/blacklists', async (req, res) => {
   const { accountAddress, name,identificationNumber } = req.body;
   try {
-    console.log(accountAddress, name,identificationNumber);
     await blackListCustomer(accountAddress, name,identificationNumber);
     res.status(200).json(req.body);
   } catch (err) {
@@ -17,22 +16,20 @@ router.post('/blacklistedCustomers', async (req, res) => {
 });
 
 
-router.get('/blacklistedCustomers', async (req, res) => {
+router.get('/blacklists', async (req, res) => {
   try {
     let response = await getAllBlackListedCustomers();
-    console.log(response);
     res.status(200).json(response);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
-router.post('/whiteListedCustomers', async (req, res) => {
+router.post('/whiteLists', async (req, res) => {
 
-  const { accountAddress, name,identificationNumber } = req.body;
-  console.log('accountAddress, name,identificationNumber',accountAddress, name,identificationNumber);
-  try {
-    console.log(accountAddress, name,identificationNumber);
+
+ try {
+    const { accountAddress, name,identificationNumber } = req.body;
     await whiteListCustomer(accountAddress, name,identificationNumber);
     res.status(200).json(req.body);
   } catch (err) {
@@ -40,19 +37,43 @@ router.post('/whiteListedCustomers', async (req, res) => {
   }
 });
 
-router.get('/whiteListedCustomers', async (req, res) => {
+router.get('/whiteLists', async (req, res) => {
   try {
     let response = await getAllwhiteListCustomers();
-    console.log(response);
     res.status(200).json(response);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
+router.get('/whiteLists/:accountAddress', async (req, res) => {
+  try {
+    let accountAddress = req.params.accountAddress;
+    let response = await checkIsWhiteListed(accountAddress);
+    res.status(200).json(response);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
+router.delete('/whitelists/:accountAddress', async (req, res) => {
+  try {
+    let accountAddress = req.params.accountAddress;
+    await removeFromWhiteListedCustomer(accountAddress);
+    res.status(200).json(req.params.accountAddress);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
-
-
+router.delete('/blacklists/:accountAddress', async (req, res) => {
+  try {
+    let accountAddress = req.params.accountAddress;
+    await removeFromBlackListedCustomer(accountAddress);
+    res.status(200).json(req.params.accountAddress);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 export default router;

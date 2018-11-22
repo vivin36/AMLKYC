762011b568f5f71f563d6ca8757d1ec6250184e5
Vivin Prasannaa <Vivin.Prasannaa@gds.ey.com>
2018-11-22 14:41:36 +0530

    ScreeningList API without getall for whitelist and blacklist

--- Java/amlkyc/src/main/java/com/blockchain/api/ScreeninglistController.java
+++ Java/amlkyc/src/main/java/com/blockchain/api/ScreeninglistController.java
@@ -1,8 +1,12 @@
 package com.blockchain.api;
 
+import java.util.List;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -10,23 +14,60 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.blockchain.business.service.IScreeninglistService;
-import com.blockchain.vo.CustomerVO;
 import com.blockchain.vo.ScreeninglistVO;
 
 @Component
 @Path("/screeninglists")
 public class ScreeninglistController {
-	
+
 	@Autowired
-	private IScreeninglistService  screeninglistService;
-	
+	private IScreeninglistService screeninglistService;
+
 	@Path("/blacklists")
 	@POST
-	@Consumes({MediaType.APPLICATION_JSON})
-	@Produces({MediaType.APPLICATION_JSON})
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
 	public ScreeninglistVO addtoBlackListCustomer(ScreeninglistVO screeninglistVO) {
 		return screeninglistService.addToBlackListedCustomer(screeninglistVO);
 	}
+
+	@Path("/blacklists")
+	@GET
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public List<ScreeninglistVO> getAllblackListCustomers() {
+		return screeninglistService.getAllblackListCustomers();
+	}
+
+	@Path("/whitelists")
+	@GET
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public List<ScreeninglistVO> getAllwhiteListCustomers() {
+		return screeninglistService.getAllwhiteListCustomers();
+	}
+
+	@Path("/whitelists")
+	@POST
+	@Consumes({MediaType.APPLICATION_JSON})
+	@Produces({MediaType.APPLICATION_JSON})
+	public ScreeninglistVO createWhiteListCustomers(ScreeninglistVO screeninglistVO) {
+		return screeninglistService.addToWhiteListedCustomer(screeninglistVO);	
+	}
 	
-	 
+	@Path("/whitelists/{accountAddress}")
+	@GET
+	@Produces({MediaType.APPLICATION_JSON})
+	public boolean checkIsWhiteListed(@PathParam("accountAddress") String accountAddress) {
+		return screeninglistService.checkIsWhiteListed(accountAddress);
+	}
+	
+	@Path("/blacklists/{accountAddress}")
+	@GET
+	@Produces({MediaType.APPLICATION_JSON})
+	public boolean checkIsBlacklists(@PathParam("accountAddress") String accountAddress) {
+		return screeninglistService.checkIsBlackListed(accountAddress);
+	}
+
+
 }
--- Java/amlkyc/src/main/java/com/blockchain/business/service/impl/ScreeninglistServiceImpl.java
+++ Java/amlkyc/src/main/java/com/blockchain/business/service/impl/ScreeninglistServiceImpl.java
@@ -1,9 +1,15 @@
 package com.blockchain.business.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.web3j.protocol.core.RemoteCall;
+import org.web3j.tuples.generated.Tuple3;
 import org.web3j.utils.Numeric;
 
+import com.blockchain.api.ScreeninglistController;
 import com.blockchain.business.service.IScreeninglistService;
 import com.blockchain.contracts.Screeninglist;
 import com.blockchain.protocol.web3.ContractsDeployer;
@@ -21,10 +27,67 @@ public class ScreeninglistServiceImpl implements IScreeninglistService {
 	public ScreeninglistVO addToBlackListedCustomer(ScreeninglistVO screeninglistVO) {
 		Screeninglist ScreeninglistContract = contractsDeployer.getScreeningListContract();
 		
-		ScreeninglistContract.addBlackListedCustomer(screeninglistVO.getAccountAddress(),
-				Numeric.hexStringToByteArray(Utils.ASCIIToHex(screeninglistVO.getIdentificationNumber())), 
-				Numeric.hexStringToByteArray(Utils.ASCIIToHex(screeninglistVO.getName())));
+		try {
+			ScreeninglistContract.addBlackListedCustomer(screeninglistVO.getAccountAddress(),
+					Numeric.hexStringToByteArray(Utils.ASCIIToHex(screeninglistVO.getIdentificationNumber())), 
+					Numeric.hexStringToByteArray(Utils.ASCIIToHex(screeninglistVO.getName()))).send();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return screeninglistVO;
+	}
+
+
+	@Override
+	public ScreeninglistVO addToWhiteListedCustomer(ScreeninglistVO screeninglistVO) {
+		Screeninglist ScreeninglistContract = contractsDeployer.getScreeningListContract();
+		
+		try {
+			ScreeninglistContract.addWhiteListedCustomer(screeninglistVO.getAccountAddress(),
+					Numeric.hexStringToByteArray(Utils.ASCIIToHex(screeninglistVO.getIdentificationNumber())), 
+					Numeric.hexStringToByteArray(Utils.ASCIIToHex(screeninglistVO.getName()))).send();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return screeninglistVO;
+	}
+
+
+	@Override
+	public List<ScreeninglistVO> getAllwhiteListCustomers() {
+		return null;
+	}
+
+
+	@Override
+	public List<ScreeninglistVO> getAllblackListCustomers() {
+		return null;
+	}
+
+
+	@Override
+	public boolean checkIsWhiteListed(String accountAddress) {
+		Screeninglist ScreeninglistContract = contractsDeployer.getScreeningListContract();
+		Boolean response = false;
+		try {
+			response = ScreeninglistContract.checkIsWhiteListed(accountAddress).send();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return response;
+	}
+
+
+	@Override
+	public boolean checkIsBlackListed(String accountAddress) {
+		Screeninglist ScreeninglistContract = contractsDeployer.getScreeningListContract();
+		Boolean response = false;
+		try {
+			response = ScreeninglistContract.checkIsBlackListed(accountAddress).send();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return response;
 	}
 
 }

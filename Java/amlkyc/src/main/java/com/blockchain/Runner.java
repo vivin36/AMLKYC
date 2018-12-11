6d6098cc05d3245f93f3155e07777af030617fd5
Abhinaba.Das@xe04.ey.com <Abhinaba.Das@xe04.ey.com>
2018-12-11 12:30:28 +0530

    Logging implemented

--- Java/amlkyc/src/main/java/com/blockchain/Runner.java
+++ Java/amlkyc/src/main/java/com/blockchain/Runner.java
@@ -1,5 +1,7 @@
 package com.blockchain;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -11,7 +13,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class Runner extends SpringBootServletInitializer {
 	
+	private static Logger logger = LogManager.getLogger(Runner.class);
+	
 	public static void main(String[] args) {
+		logger.info("Application being started!!!");
 		new Runner().configure(new SpringApplicationBuilder(Runner.class)).run(args);
 	}
 }
